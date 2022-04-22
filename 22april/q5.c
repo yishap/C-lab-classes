909517ff70c329c4eac3d459f43d22d2c72a464c
yishap <104184446+yishap@users.noreply.github.com>
2022-04-22 10:43:34 +0530

    Add files via upload

---
+++ 22april/q5.c
@@ -0,0 +1,38 @@
+// display   1   2   3   4   5
+//           9   8   7   6
+//           10  11  12
+//           14  13
+//           15
+#include<stdio.h>
+int main()
+{
+	int n=5, a=1, b=1, i=0;
+	loop:
+		if(n%2!=0)
+		{
+		b=1;
+		x:
+			printf("%d \t",a);
+			a++;
+			b++;
+			if(k<=n)
+		goto x;
+		}
+		else
+		{
+			int d = a;
+			i = b+n-1;
+			y:	
+				printf("%d \t", i);	
+				a++;
+				i--;
+				if(i>=d)
+			goto y;
+		}
+	printf("\n");
+	n--;
+	if(n>=1)
+	goto loop;
+}
+	
+	
